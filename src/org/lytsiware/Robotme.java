c3cb585d960ec9ab3dd5b2a6a00ea9d480625101
Lytsikas, Christos <christos.lytsikas@arhs-dev-hellas.com>
2023-02-06 15:06:03 +0200

    Do not move mouse if it is already moved

--- src/org/lytsiware/Robotme.java
+++ src/org/lytsiware/Robotme.java
@@ -4,19 +4,31 @@ import java.awt.*;
 
 public class Robotme {
 
+    private static final int MAX_IDLE_TIME_MILLIS = 2 * 60 * 1000;
+
+
     public void run() throws AWTException {
         GraphicsEnvironment graphicEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice screenDevice = graphicEnv.getDefaultScreenDevice();
         Robot robot = new Robot(screenDevice);
+        Point oldMouseLocation = MouseInfo.getPointerInfo().getLocation();
+        long mouseMovedOn = System.currentTimeMillis();
+
         while (true) {
+            robot.delay(5 * 1000);
             Point currentMouseLocation = MouseInfo.getPointerInfo().getLocation();
-            int nextLocationX = currentMouseLocation.getX() > 100 ? 90 : 500;
-            robot.delay(30 * 1000);
-            robot.waitForIdle();
-            robot.mouseMove(nextLocationX, 100);
+            if (!oldMouseLocation.equals(currentMouseLocation)) {
+                oldMouseLocation = currentMouseLocation;
+                mouseMovedOn = System.currentTimeMillis();
+            } else if (System.currentTimeMillis() - mouseMovedOn > MAX_IDLE_TIME_MILLIS) {
+                int nextLocationX = (int) currentMouseLocation.getX() + (Math.random() * 10 > 0.5 ? 1 : -1);
+                robot.waitForIdle();
+                robot.mouseMove(nextLocationX, 100);
+                oldMouseLocation = currentMouseLocation;
+                mouseMovedOn = System.currentTimeMillis();
+            }
         }
     }
-
 
 
     public static void main(String[] args) throws AWTException {
