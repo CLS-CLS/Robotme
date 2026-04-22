631ee0e3d9524bcc104dd23c6e36faf1ee5e7966
Chris <ChrisLytsikas@gmail.com>
2026-04-22 10:50:33 +0300

    When daytime is provided, robot will stop moving the mouse after that time

--- src/org/lytsiware/Robotme.java
+++ src/org/lytsiware/Robotme.java
@@ -1,37 +1,52 @@
 package org.lytsiware;
 
 import java.awt.*;
+import java.time.LocalTime;
 
 public class Robotme {
 
     private static final int MAX_IDLE_TIME_MILLIS = 2 * 60 * 1000;
 
 
-    public void run() throws AWTException {
-        GraphicsEnvironment graphicEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice screenDevice = graphicEnv.getDefaultScreenDevice();
+    public void run(LocalTime endTime) throws AWTException {
+        GraphicsDevice screenDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         Robot robot = new Robot(screenDevice);
+
         Point oldMouseLocation = MouseInfo.getPointerInfo().getLocation();
         long mouseMovedOn = System.currentTimeMillis();
 
-        while (true) {
+        while (before(endTime)) {
             robot.delay(5 * 1000);
             Point currentMouseLocation = MouseInfo.getPointerInfo().getLocation();
+            // mouse moved, update the new time and position
             if (!oldMouseLocation.equals(currentMouseLocation)) {
                 oldMouseLocation = currentMouseLocation;
                 mouseMovedOn = System.currentTimeMillis();
-            } else if (System.currentTimeMillis() - mouseMovedOn > MAX_IDLE_TIME_MILLIS) {
+            }
+            // mouse not moved, if mouse has not moved for than MAX_IDLE_TIME, make it move manually
+            else if (System.currentTimeMillis() - mouseMovedOn > MAX_IDLE_TIME_MILLIS) {
                 int nextLocationX = (int) currentMouseLocation.getX() + (Math.random() * 10 > 0.5 ? 1 : -1);
                 robot.waitForIdle();
-                robot.mouseMove(nextLocationX, 100);
+                robot.mouseMove(nextLocationX, currentMouseLocation.y);
                 oldMouseLocation = currentMouseLocation;
                 mouseMovedOn = System.currentTimeMillis();
             }
         }
     }
 
+    private boolean before(LocalTime endTime) {
+        if (endTime == null) {
+            return true;
+        }
+        return LocalTime.now().isBefore(endTime);
+    }
+
 
     public static void main(String[] args) throws AWTException {
-        new Robotme().run();
+        LocalTime time = null;
+        if (args != null && args.length >=1) {
+            time = LocalTime.parse(args[0]);
+        }
+        new Robotme().run(time);
     }
 }
